f2ee3725d7b9eaa91293ca551c10bd63fd46e542
andoryupurfire <anferoro2004@gmail.com>
2026-05-03 11:48:58 -0500

    Spring inicializado

---
+++ agro_plus/src/main/java/com/agro/agro_plus/AgroPlusApplication.java
@@ -0,0 +1,13 @@
+package com.agro.agro_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroPlusApplication.class, args);
+	}
+
+}
---
+++ agro_plus/src/test/java/com/agro/agro_plus/AgroPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agro.agro_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
